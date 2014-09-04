319e685a2af3c7ea294c8fb77119ca0169bc07fa
birdlab <mail@birdlab.ru>
2014-09-05 02:52:19 +0400

    Production sync

---
+++ src/www/invites/config.php
@@ -0,0 +1,10 @@
+<?php
+$__mysql_config = array (
+					'server' => 'localhost',
+					'username' => 'root',
+					'password' => 'looping',
+					'db' => 'trigger',
+					'port' => '3306',
+					'charset' => 'utf8',
+				);
+?>
